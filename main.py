300b817b1ca90b2a9cb7b91b80673868b67c7a85
Wontez Morgan <wontez@protonmail.com>
2018-11-26 21:51:46 -0500

    Created the script that can get the website and parse metadata about the author of the site.

--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 # This script will receive an online source and create the proper citations
 # in APA format.
 #
+# Depends on wget, libextractor, extract, BibTex
+#
 # Created by Jennifer and Wontez Morgan -- 25 November 2018
 
 
 # Modules to be imported
 import subprocess
-
+import os
 
 # Declare Global Variables
 lastName = 'Morgan'
-
+devnull = open(os.devnull, 'w')
 
 
 # Clear the screen
@@ -29,6 +31,6 @@ print (citeURL)
 # *figure out a way to do this without using wget in the 
 # future.
 
-subprocess.call('wget '+ citeURL,shell=True)
+subprocess.call('wget -O tempSource '+ citeURL,stdout=devnull,stderr=devnull,shell=True)
 
-
+subprocess.call('extract tempSource ',shell=True)
